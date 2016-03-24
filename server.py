76640ed0f3d938f55a3854fb8c27397735f470dd
Ryan Eagan <eaganry@gmail.com>
2016-03-24 14:35:30 -0400

    Basic server/frontend communcation for gamestate added a few more cards/images

--- server.py
+++ server.py
@@ -15,9 +15,10 @@ Read about it online.
 """
 
 import os
+import json
 from sqlalchemy import *
 from sqlalchemy.pool import NullPool
-from flask import Flask, request, render_template, g, redirect, Response
+from flask import Flask, request, render_template, g, redirect, Response, send_from_directory
 
 tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
 app = Flask(__name__, template_folder=tmpl_dir)
@@ -41,16 +42,6 @@ DATABASEURI = "postgresql://rse2119:1437@w4111a.eastus.cloudapp.azure.com/proj1p
 # This line creates a database engine that knows how to connect to the URI above.
 #
 engine = create_engine(DATABASEURI)
-
-#
-# Example of running queries in your database
-# Note that this will probably not work if you already have a table named 'test' in your database, containing meaningful data. This is only an example showing you how to run queries in your database using SQLAlchemy.
-#
-engine.execute("""CREATE TABLE IF NOT EXISTS test (
-  id serial,
-  name text
-);""")
-engine.execute("""INSERT INTO test(name) VALUES ('grace hopper'), ('alan turing'), ('ada lovelace');""")
 
 
 @app.before_request
@@ -80,79 +71,40 @@ def teardown_request(exception):
   except Exception as e:
     pass
 
-
-#
-# @app.route is a decorator around index() that means:
-#   run index() whenever the user tries to access the "/" path using a GET request
-#
-# If you wanted the user to go to, for example, localhost:8111/foobar/ with POST or GET then you could use:
-#
-#       @app.route("/foobar/", methods=["POST", "GET"])
-#
-# PROTIP: (the trailing / in the path is important)
-# 
-# see for routing: http://flask.pocoo.org/docs/0.10/quickstart/#routing
-# see for decorators: http://simeonfranklin.com/blog/2012/jul/1/python-decorators-in-12-steps/
-#
 @app.route('/')
 def index():
-  """
-  request is a special object that Flask provides to access web request information:
+  context = {}
 
-  request.method:   "GET" or "POST"
-  request.form:     if the browser submitted a form, this contains the data in the form
-  request.args:     dictionary of URL arguments, e.g., {a:1, b:2} for http://localhost?a=1&b=2
-
-  See its API: http://flask.pocoo.org/docs/0.10/api/#incoming-request-data
-  """
-
-  # DEBUG: this is debugging code to see what request looks like
-  print request.args
+  return render_template("index.html", **context)
 
 
-  #
-  # example of a database query
-  #
-  cursor = g.conn.execute("SELECT name FROM test")
-  names = []
-  for result in cursor:
-    names.append(result['name'])  # can also be accessed using result[0]
+@app.route('/gamestate', methods=['POST'])
+def gamestate():
+  cursor = g.conn.execute("SELECT * FROM cards_in_play a JOIN all_cards b ON a.card_name = b.card_name;")
+  result = {}
+  columns = cursor.keys()
+  for row in cursor:
+    data = {}
+    for i in range(1, len(row)):
+      data[columns[i]] = row[i]
+    result[row[0]] = data
   cursor.close()
 
-  #
-  # Flask uses Jinja templates, which is an extension to HTML where you can
-  # pass data to a template and dynamically generate HTML based on the data
-  # (you can think of it as simple PHP)
-  # documentation: https://realpython.com/blog/python/primer-on-jinja-templating/
-  #
-  # You can see an example template in templates/index.html
-  #
-  # context are the variables that are passed to the template.
-  # for example, "data" key in the context variable defined below will be 
-  # accessible as a variable in index.html:
-  #
-  #     # will print: [u'grace hopper', u'alan turing', u'ada lovelace']
-  #     <div>{{data}}</div>
-  #     
-  #     # creates a <div> tag for each element in data
-  #     # will print: 
-  #     #
-  #     #   <div>grace hopper</div>
-  #     #   <div>alan turing</div>
-  #     #   <div>ada lovelace</div>
-  #     #
-  #     {% for n in data %}
-  #     <div>{{n}}</div>
-  #     {% endfor %}
-  #
-  context = dict(data = names)
+  return Response(response=json.dumps(result), status=200, mimetype="application/json")
+
+@app.route('/js/<path:path>')
+def js(path):
+  return send_from_directory('templates/js', path)
 
 
-  #
-  # render_template looks in the templates/ folder for files.
-  # for example, the below file reads template/index.html
-  #
-  return render_template("index.html", **context)
+@app.route('/img/<path:path>')
+def img(path):
+  return send_from_directory('templates/img', path)
+
+
+@app.route('/css/<path:path>')
+def css(path):
+  return send_from_directory('templates/css', path)
 
 #
 # This is an example of a different path.  You can see it at:
--- templates/js/dominion.js
+++ templates/js/dominion.js
@@ -1,13 +1,43 @@
-var hand = ["Estate", "Estate", "Copper", "Estate", "Copper"];
-var actions = {"Market":10, "Village":10, "Woodcutter":10, "Festival":10, "Moat":10};
+var actions = {};
+var treasures = {};
+var estates = {};
 
-//setup hand
-for(var i=0;i<5 && i<hand.length;i++) {
-	$(".hand .card")[i].className = "card " + hand[i];
-}
 
-//setup actions
-var ac = Object.keys(actions);
-for(var i=0;i<10 && i<ac.length;i++) {
-	$(".actions .card")[i].className = "card " + ac[i];
-}
+$.ajax({
+  type : "POST",
+  url : "/gamestate",
+  data: {},
+  success:function (data) {
+    console.log(data);
+    for(var card in data) {
+      if(data[card]["is_treasure"]) treasures[card] = data[card];
+      else if(data[card]["victory_point_value"] > 0) estates[card] = data[card];
+      else actions[card] = data[card];
+    }
+
+
+    var hand = ["Estate", "Estate", "Copper", "Moat", "Copper"];
+
+    //setup hand
+    var handcards = document.querySelectorAll(".hand .card");
+    for(var i=0;i<5 && i<hand.length;i++) {
+      handcards[i].className = "card " + hand[i];
+    }
+
+    //setup actions
+    var ac = Object.keys(actions);
+    var actioncards = document.querySelectorAll(".actions .card");
+    for(var i=0;i<10 && i<ac.length;i++) {
+      actioncards[i].className = "card " + ac[i].replace(' ', '-');
+    }
+  
+
+
+  },
+  error: function(err) {
+    console.log("error");
+    console.log(err);
+  }
+
+});
+
