866786ed5d8e5c7432a2a85548981a6b32e601ed
Ryan Eagan <eaganry@gmail.com>
2016-03-26 15:30:47 -0400

    board and hand show all card attributes disabled background pictures on cards for now

--- templates/js/dominion.js
+++ templates/js/dominion.js
@@ -1,7 +1,40 @@
+var playerid = 0;
+
 var actions = {};
+var actionkeys = [];
 var treasures = {};
 var estates = {};
+var tekeys = ["Gold", "Silver", "Copper", "Province", "Duchy", "Estate"];
 
+
+function drawcards(selector, cardnames, cards, hand) {
+    //setup actions
+    var selected = document.querySelectorAll(selector);
+    for(var i=0;i<10 && i<cardnames.length;i++) {
+      selected[i].className = "card " + cardnames[i].replace(' ', '-');
+      var card = cards[cardnames[i]];
+      var text = "<div class=\"card-name\">" + card["card_name"] + "</div>";
+      var count = 0;
+      if(card["plus_card"] > 0) { text += "<div>+" + card["plus_card"] + " Cards</div>"; count++ }
+      if(card["plus_action"] > 0) { text += "<div>+" + card["plus_action"] + " Actions</div>"; count ++ }
+      if(card["plus_buy"] > 0) { text += "<div>+" + card["plus_buy"] + " Buys</div>"; count ++ }
+      if(card["coin_value"] > 0) { text += "<div>+$" + card["coin_value"] + "</div>"; count ++ }
+      if(card["plus_victory_point"] > 0) { text += "<div>+" + card["plus_victory_point"] + " VP</div>"; count++ }
+      if(card["victory_point_value"] > 0) { text += "<div>Worth " + card["victory_point_value"] + " VP</div>"; count++ }
+
+      while(count < 4) {
+        text += "<div>&nbsp;</div>";
+        count++;
+      }
+      if(!hand) {
+        text += "<div>Costs $"+card["cost"]+"</div>"; 
+        text += "<div>"+card["num_cards"] + " remaining</div>";
+      }
+
+      selected[i].innerHTML = text;
+    }
+
+}
 
 $.ajax({
   type : "POST",
@@ -15,23 +48,19 @@ $.ajax({
       else actions[card] = data[card];
     }
 
-
     var hand = ["Estate", "Estate", "Copper", "Moat", "Copper"];
 
     //setup hand
-    var handcards = document.querySelectorAll(".hand .card");
-    for(var i=0;i<5 && i<hand.length;i++) {
-      handcards[i].className = "card " + hand[i];
-    }
+    drawcards(".hand .card", hand, data, true);
+ 
+    //actions
+    actionkeys = Object.keys(actions).sort(function(a,b) {
+      return actions[a]["cost"] > actions[b]["cost"];
+    });
+    drawcards(".actions .card", actionkeys, actions);
 
-    //setup actions
-    var ac = Object.keys(actions);
-    var actioncards = document.querySelectorAll(".actions .card");
-    for(var i=0;i<10 && i<ac.length;i++) {
-      actioncards[i].className = "card " + ac[i].replace(' ', '-');
-    }
-  
-
+    //treasures and estates
+    drawcards(".money-estates .card", tekeys, $.extend({}, treasures, estates));
 
   },
   error: function(err) {
@@ -39,5 +68,11 @@ $.ajax({
     console.log(err);
   }
 
+});
+
+$(".player-select").change(function() {
+  var val = parseInt($(this).val());
+  playerid = val;
+  
 });
 
